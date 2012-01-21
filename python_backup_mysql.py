884aba83b074887f6c04e956e7db8bff662e7b7f
alphabraga <alphabraga@atom.(none)>
2012-01-21 00:31:05 -0300

    refatorando codigo devido arquido de config e coloando traceback

--- python_backup_mysql.py
+++ python_backup_mysql.py
@@ -7,34 +7,39 @@
 #Licenca GPL    						   #
 ####################################################################
 
+import config # arquivo de configuracao
 import MySQLdb
 import os
 import datetime
-
-host     = 'hostname'     # nome ou ip do servidor do servidor Mysql
-user     = 'username'          # nome do usuario para acesso ao servidor de banco de dados
-password = 'your_pass_where'      # senha para acesso ao banco de dados
-path     = '/the/folder/name/'# Pasta onde se deseja salvar os backups dos bancos de dados
-
+import traceback
 
 now	 = datetime.datetime.now() #data atual
 
-db = MySQLdb.connect(host,user,password) #conexao com o banco
+try:
+	db = MySQLdb.connect(config.host,config.user,config.password) #conexao com o banco
 
-cursor = db.cursor() 
 
-cursor.execute("show databases") #executo um sql para retornar todos os nomes dos bancos de dados
+	cursor = db.cursor() 
+
+	cursor.execute("show databases") #executo um sql para retornar todos os nomes dos bancos de dados
  
-data = cursor.fetchall() # coloco em uma tupla
+	data = cursor.fetchall() # coloco em uma tupla
 
-# executo um mysqldump em cada banco de dados compacto em 'path'
-for item in data:
+	# executo um mysqldump em cada banco de dados compacto em 'path'
+	for item in data:
 
-	filename = path+item[0]+'.'+str(now.day)+str(now.month)+str(now.year)+'.sql.gz'	
+		filename = config.path+item[0]+'.'+str(now.day)+str(now.month)+str(now.year)+'.sql.gz'	
 
-	if item[0] != 'mysql' and item[0] != 'information_schema': 
-		os.system('mysqldump -u'+user+' -p'+password+' '+item[0]+' | gzip > '+filename)
+		if item[0] != 'mysql' and item[0] != 'information_schema': 
+			os.system('mysqldump -u'+config.user+' -p'+config.password+' '+item[0]+' | gzip > '+filename)
 		
-		size = os.path.getsize(filename) # tamanho do arquivo
-		size = size/1024*1024		 # tamanho em MB 
-		print 'Backup de '+filename+' finalizado. Tamanho do arquivo: '+str(size)+' MB' 
+			size = os.path.getsize(filename) # tamanho do arquivo
+			size = size/1024*1024		 # tamanho em MB 
+			print 'Backup de '+filename+' finalizado. Tamanho do arquivo: '+str(size)+' MB' 
+except:
+
+	print 'Check your config.py file....\n'
+	print 'host: '+config.host
+	print 'username: '+config.user
+	print 'password: '+config.password
+	print 'path: '+config.path		
