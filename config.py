f4b3bb4f309071e1674a7ed3b379117615e940ce
alphabraga <alphabraga@atom.(none)>
2012-01-21 00:30:18 -0300

    colocando arquivo de configuracao separado

---
+++ config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+host     = 'hostname'            # nome ou ip do servidor do servidor Mysql
+user     = 'username'            # nome do usuario para acesso ao servidor de banco de dados
+password = 'your-password'       # senha para acesso ao banco de dados
+path     = '/your/backup/folder/'# Pasta onde se deseja salvar os backups dos bancos de dados
