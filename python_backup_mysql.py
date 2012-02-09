8ba528fbe42368c6caf3c0eba0d08e79d8314f9b
Alfredo <alphabraga@hotmail.com>
2012-02-09 18:39:52 -0300

    colocando create database if not exists, colocando inicio e fim do processamento de cada banco colocando metodo para criacao de timestamp

--- python_backup_mysql.py
+++ python_backup_mysql.py
@@ -11,35 +11,49 @@ import config # arquivo de configuracao
 import MySQLdb
 import os
 import datetime
+import time
 import traceback
 
 now	 = datetime.datetime.now() #data atual
 
+def datetime():
+	
+	t = time.localtime();	
+	timestamp = str(t.tm_mday)+'/'+str(t.tm_mon)+'/'+ str(t.tm_year)+'  '+str(t.tm_hour)+':'+str(t.tm_min)+':'+str(t.tm_sec) 
+	return timestamp
+
 try:
 	db = MySQLdb.connect(config.host,config.user,config.password) #conexao com o banco
 
-
-	cursor = db.cursor() 
-
-	cursor.execute("show databases") #executo um sql para retornar todos os nomes dos bancos de dados
- 
-	data = cursor.fetchall() # coloco em uma tupla
-
-	# executo um mysqldump em cada banco de dados compacto em 'path'
-	for item in data:
-
-		filename = config.path+item[0]+'.'+str(now.day)+str(now.month)+str(now.year)+'.sql.gz'	
-
-		if item[0] != 'mysql' and item[0] != 'information_schema': 
-			os.system('mysqldump -u'+config.user+' -p'+config.password+' '+item[0]+' | gzip > '+filename)
-		
-			size = os.path.getsize(filename) # tamanho do arquivo
-			size = size/1024*1024		 # tamanho em MB 
-			print 'Backup de '+filename+' finalizado. Tamanho do arquivo: '+str(size)+' MB' 
 except:
 
 	print 'Check your config.py file....\n'
 	print 'host: '+config.host
 	print 'username: '+config.user
 	print 'password: '+config.password
-	print 'path: '+config.path		
+	print 'path: '+config.path
+
+cursor = db.cursor() 
+
+cursor.execute("show databases") #executo um sql para retornar todos os nomes dos bancos de dados
+
+data = cursor.fetchall() # coloco em uma tupla
+
+# executo um mysqldump em cada banco de dados e compacto em 'path'
+for item in data:
+
+	#tm_year=2012, tm_mon=2, tm_mday=9, tm_hour=18, tm_min=3, tm_sec=39, tm_wday=3, tm_yday=40, tm_isdst=0
+	filename = config.path+item[0]+'.'+str(now.day)+str(now.month)+str(now.year)+'.sql.gz'	
+
+	if item[0] != 'mysql' and item[0] != 'information_schema' and item[0] != 'performance_schema' : 
+	
+		timestamp = datetime()
+		print 'Iniciando o backup de '+item[0]+' em '+timestamp 	
+		
+		os.system('mysqldump --default-character-set=utf8 --add-drop-database -u'+config.user+' -p'+config.password+' --database '+item[0]+' | gzip > '+filename)
+
+		size = os.path.getsize(filename) # tamanho do arquivo
+		size = size/1024*1024		 # tamanho em MB
+		timestamp = datetime() 
+		print 'Backup '+filename+' finalizado em '+timestamp+'. Tamanho do arquivo: '+str(size)+' MB' 
+		
