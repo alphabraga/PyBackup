a8a3af554c05c80d24959617e7639b75d487191b
Alfredo Braga <alphabraga@hotmail.com>
2012-06-13 18:08:26 -0300

    eniando email

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
+# Arquivo de configuração do Pybackup
+#
+#
+#
+
+
 host     = 'hostname'            # nome ou ip do servidor do servidor Mysql
 user     = 'username'            # nome do usuario para acesso ao servidor de banco de dados
 password = 'your-password'       # senha para acesso ao banco de dados
---
+++ sendemail.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python2.7
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
+import os
+
+def sendMail(to, subject, text, files=[],server="localhost"):
+    assert type(to)==list
+    assert type(files)==list
+    fro = "Expediteur <expediteur@mail.com>"
+
+    msg = MIMEMultipart()
+    msg['From'] = fro
+    msg['To'] = COMMASPACE.join(to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach( MIMEText(text) )
+
+    for file in files:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload( open(file,"rb").read() )
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"'
+                       % os.path.basename(file))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server)
+    smtp.sendmail(fro, to, msg.as_string() )
+    smtp.close()
+
+
+sendMail(
+        ["destination@dest.kio"],
+        "hello","cheers",
+        ["photo.jpg","memo.sxw"]
+    )
+
